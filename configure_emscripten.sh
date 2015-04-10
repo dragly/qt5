06234dec0413c601aad72b95d6efa4febc97c39c
Svenn-Arne Dragly <s@dragly.com>
2015-04-10 08:51:04 +0200

    Updated configure script for emscripten
    Change-Id: I99e958007224d5f46b6bdacb7cd859506c5fb5d6

--- configure_emscripten.sh
+++ configure_emscripten.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-./configure -xplatform web-emcc -developer-build -opensource -nomake examples -nomake tests -skip qttranslations -skip qtserialport -skip qtwebkit-examples -no-openssl -static -system-zlib -no-gif -no-xcb -qt-zlib -qt-libpng -qt-libjpeg -no-accessibility -dbus -no-sse2 -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-icu -no-rpath  -confirm-license -nomake examples -no-feature-codecs -no-feature-printpreviewwidget  -no-feature-printpreviewdialog  -no-feature-systemsemaphore -no-feature-sharedmemory -make tools -no-freetype -v
+./configure -xplatform web-emcc -developer-build -opensource -static -nomake examples -nomake tests -nomake tools -skip qttranslations -skip qtserialport -skip qtwebkit-examples -no-openssl -no-freetype -system-zlib -no-gif -no-xcb -qt-zlib -qt-libpng -qt-libjpeg -no-accessibility -dbus -no-sse2 -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-icu -no-rpath  -confirm-license -nomake examples -no-feature-codecs -no-feature-printpreviewwidget  -no-feature-printpreviewdialog  -no-feature-systemsemaphore -no-feature-sharedmemory
+
+# -v
